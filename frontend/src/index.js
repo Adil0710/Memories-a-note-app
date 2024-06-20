95310230132a25cceebceed7511909e54dd692fc
Adil0710 <padil2246@gmail.com>
2024-06-20 16:59:25 +0530

    fourth commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App';
-import axios from 'axios';
-import { ThemeProvider } from './context/ThemeProvider';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./components/App";
+import axios from "axios";
+import { ThemeProvider } from "./context/ThemeProvider";
 
-axios.defaults.baseURL = 'http://localhost:3000'
-axios.defaults.withCredentials = true
+const apiUrl = process.env.REACT_APP_API_URL;
+console.log('API URL:', apiUrl); // Debugging line to check if the URL is loaded
+console.log(process.env);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+axios.defaults.baseURL = `${apiUrl}`;
+axios.defaults.withCredentials = true;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  
-    <ThemeProvider>
-        <App />
-    </ThemeProvider>,
-  
+  <ThemeProvider>
+    <App />
+  </ThemeProvider>
 );
-
