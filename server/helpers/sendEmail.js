096ccd7aad8ceda7fe9fc30601a7ce1402df4708
Adil0710 <padil2246@gmail.com>
2024-06-22 01:36:54 +0530

    added signup welcome mail with port

--- server/helpers/sendEmail.js
+++ server/helpers/sendEmail.js
@@ -5,6 +5,7 @@ dotenv.config();
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
+  port: 587,
   secure: false, // Use true for port 465, false for all other ports
   auth: {
     user: process.env.EMAIL_USER, // Use environment variables for sensitive data
