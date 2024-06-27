a1440ebc4765c4d4f45e4a619f55090f88542556
Adil0710 <padil2246@gmail.com>
2024-06-28 03:10:26 +0530

    footer color

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -7,7 +7,7 @@ import { FaGithub, FaInstagram, FaLinkedin, FaXTwitter } from "react-icons/fa6";
 function Footer() {
   const {isDarkMode} = useTheme()
   return (
-    <footer className=" bg-gray-100 dark:bg-black py-6 text-center lg:px-19 sm:px-10 px-2  text-[#494949] dark:text-gray-300">
+    <footer className=" bg-gray-100 dark:bg-black py-6 text-center lg:px-19 sm:px-10 px-2  text-[#494949] dark:text-white dark:text-opacity-60">
       <ConfigProvider
           theme={{
           algorithm: isDarkMode ? theme.darkAlgorithm : theme.defaultAlgorithm,
@@ -23,9 +23,9 @@ function Footer() {
 
             <div className=' flex items-center justify-center gap-5 text-xl'>
             <a href='https://www.instagram.com/code_with_adil?igsh=NTc2dTRudnc1ZnYz' target='_blank'><FaInstagram className='hover:text-rose-600 transition-all duration-200'/></a> 
-              <a href='https://x.com/AdilPat21587273' target='_blank'><FaXTwitter className=' dark:hover:text-gray-400 hover:text-black transition-all duration-200'/></a>
+              <a href='https://x.com/AdilPat21587273' target='_blank'><FaXTwitter className=' dark:hover:text-gray-50 hover:text-black transition-all duration-200'/></a>
               <a href='https://www.linkedin.com/in/adil-patel-737692252/' target='_blank'><FaLinkedin className=' hover:text-blue-500 transition-all duration-200'/></a>
-              <a href='https://github.com/adil0710' target='_blank'><FaGithub className=' dark:hover:text-gray-400 hover:text-black transition-all duration-200'/></a>
+              <a href='https://github.com/adil0710' target='_blank'><FaGithub className=' dark:hover:text-gray-50 hover:text-black transition-all duration-200'/></a>
             </div>
 
           </div>
@@ -33,7 +33,7 @@ function Footer() {
       <Divider/>
         
         <div className=' flex items-center justify-center text-md'>
-          <p>Developed by</p> <Divider type='vertical' className=' h-5'/> <a className=' font-semibold hover:text-black dark:hover:text-white transition-all duration-200' href='https://adil0710.github.io' target='_blank'>Adil Patel</a>
+          <p>Developed by</p> <Divider type='vertical' className=' h-5'/> <a className=' font-semibold hover:text-black dark:hover:text-white dark:text-white dark:text-opacity-80 transition-all duration-200' href='https://adil0710.github.io' target='_blank'>Adil Patel</a>
         </div>
       </ConfigProvider>
   </footer>
